e9bb574e3db8df596e6ef9dca0c94511a199cc87
Natasha Bandzeladze <na.bandzeladze@gmail.com>
2022-10-06 00:42:17 +0300

    feature: add mobile adaptation for section Case-Studies

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,10 +3,20 @@ $(document).ready(function(){
         dots: false,
         margin: 48,
         loop: true,
+        responsive: {
+            0 : {
+                items: 2,
+                margin: 32,
+            },
+            601 : {
+                items: 3,
+                margin: 32,
+            },
+        }
     });
   });
 
-  /* Visdeo */
+  /* Video */
   const videoButton = document.querySelector('#video-btn');
   const videoImg = document.querySelector('.about__img');
   const videoWrapper = document.querySelector('.about__video');
