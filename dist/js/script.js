9125bcb3a5f6fa1e7a42dabf0c51029645c4d911
Natasha Bandzeladze <na.bandzeladze@gmail.com>
2022-10-05 18:54:03 +0300

    feature: create section Video

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,3 +5,22 @@ $(document).ready(function(){
         loop: true,
     });
   });
+
+  /* Visdeo */
+  const videoButton = document.querySelector('#video-btn');
+  const videoImg = document.querySelector('.about__img');
+  const videoWrapper = document.querySelector('.about__video');
+  const video = document.querySelector('#video-file');
+
+  videoWrapper.addEventListener('click', function() {
+    if (video.paused) {
+        videoImg.classList.add('none');
+        videoButton.classList.add('none');
+        videoWrapper.classList.remove('about__video-overlay');
+        video.play();
+    } else {
+        videoButton.classList.remove('none');
+        videoWrapper.classList.add('about__video-overlay');
+        video.pause();
+    }
+  })
