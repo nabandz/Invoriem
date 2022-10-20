92eedde5a8273619df9c6d97eaf4d405fa9c61e5
Natasha Bandzeladze <na.bandzeladze@gmail.com>
2022-10-20 19:26:44 +0300

    feature: add script for mobile navigation

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,23 +14,38 @@ $(document).ready(function(){
             },
         }
     });
-  });
+});
 
   /* Video */
-  const videoButton = document.querySelector('#video-btn');
-  const videoImg = document.querySelector('.about__img');
-  const videoWrapper = document.querySelector('.about__video');
-  const video = document.querySelector('#video-file');
+const videoButton = document.querySelector('#video-btn');
+const videoImg = document.querySelector('.about__img');
+const videoWrapper = document.querySelector('.about__video');
+const video = document.querySelector('#video-file');
 
-  videoWrapper.addEventListener('click', function() {
-    if (video.paused) {
-        videoImg.classList.add('hidden');
-        videoButton.classList.add('none');
-        videoWrapper.classList.remove('about__video-overlay');
-        video.play();
-    } else {
-        videoButton.classList.remove('none');
-        videoWrapper.classList.add('about__video-overlay');
-        video.pause();
-    }
-  })
+videoWrapper.addEventListener('click', function() {
+if (video.paused) {
+    videoImg.classList.add('hidden');
+    videoButton.classList.add('none');
+    videoWrapper.classList.remove('about__video-overlay');
+    video.play();
+} else {
+    videoButton.classList.remove('none');
+    videoWrapper.classList.add('about__video-overlay');
+    video.pause();
+}
+});
+
+/* Mobile Nav */
+const openNavbtn = document.querySelector('#openMobileNav');
+const closeNavbtn = document.querySelector('#closeMobileNav');
+const mobileNav = document.querySelector('#mobileNav');
+
+openNavbtn.onclick = function() {
+    mobileNav.classList.remove('none');
+    document.body.classList.add('no-scroll');
+}
+
+closeNavbtn.onclick = function() {
+    mobileNav.classList.add('none');
+    document.body.classList.remove('no-scroll');
+}
